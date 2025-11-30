fa01fb1ac7a2871e79388d5ecc0a92230806e4d8
Shaikh Saif <shaikhsaif20010622@gmail.com>
2025-11-30 14:44:33 +0530

    Added Cors middleware.

--- Redis/Program.cs
+++ Redis/Program.cs
@@ -7,12 +7,45 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddControllersWithViews();
 
+
+// bydefault allows to all u need to handle this in your code.explore-->httpoptions in rest apis
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+//for specific websites
+
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("FacebookOnly", policy =>
+//    {
+//        policy.WithOrigins(
+//                "https://www.facebook.com",
+//                "https://facebook.com",
+//                "https://m.facebook.com",
+//                "https://business.facebook.com",
+//                "https://developers.facebook.com"
+//            )
+//            .AllowAnyMethod()
+//            .AllowAnyHeader()
+//            .AllowCredentials(); // Important for session cookies
+//    });
+//});
+
 // Configure Redis for session storage
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = "localhost:6379"; // Your Redis connection string
     options.InstanceName = "SessionDemo_";
 });
+
+
 
 // Configure session
 builder.Services.AddSession(options =>
